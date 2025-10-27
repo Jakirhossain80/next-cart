0483db3392139de4362f477894609bc5890ec532
JakirHossain <jakir.devbd@gmail.com>
2025-10-27 09:19:45 +0600

    Layout has been modified for seo and proer typescript syntex

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Button>Click Me</Button>
+      <p className='text-xl text-red-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ratione fuga assumenda voluptas, voluptate sequi nihil iste commodi consequuntur fugiat ullam. Saepe eaque, perspiciatis nisi laboriosam impedit optio quas, quasi minima unde vel dolorum aut assumenda sequi ea hic? Beatae cupiditate pariatur veniam omnis sequi vero! Mollitia laudantium nisi laboriosam.</p>
+      
+      </div>
   )
 }
 
