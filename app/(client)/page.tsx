d8f3605b76211225c299c183f45652ac3f879f06
JakirHossain <jakir.devbd@gmail.com>
2025-10-30 23:48:20 +0600

    slug page done

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,16 +1,20 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
+import { getCategories } from "@/sanity/queries";
 
-
-
-const Home = () => {
+const Home = async () => {
+  const categories = await getCategories(6);
+  
   return (
     <Container className="bg-shop-light-pink">
-      <HomeBanner/>
-      <ProductGrid/>
+      <HomeBanner />
+      <ProductGrid />
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
 
 export default Home;
+
