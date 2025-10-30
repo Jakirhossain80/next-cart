6bd3df58b53dc30159e300469cc0a74e32255071
JakirHossain <jakir.devbd@gmail.com>
2025-10-30 11:03:31 +0600

    Home categories component has been done

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 
 
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <Container className="bg-shop-light-pink">
       <HomeBanner/>
+      <ProductGrid/>
     </Container>
   );
 };
