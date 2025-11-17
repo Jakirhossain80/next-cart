8a7d9bb72eb985a4c861f59e670fb41d50a24967
JakirHossain <jakir.devbd@gmail.com>
2025-11-17 22:25:18 +0600

    ShopPage has been updated

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,13 +1,26 @@
+// app/(client)/shop/page.tsx
+import { Suspense } from "react";
 import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
+
+// Force this route to be dynamic (Clerk + search params, etc.)
+export const dynamic = "force-dynamic";
 
 const ShopPage = async () => {
   const categories = await getCategories();
   const brands = await getAllBrands();
+
   return (
     <div className="bg-white">
-      <Shop categories={categories} brands={brands} />
+      <Suspense
+        fallback={
+          <div className="min-h-[400px] flex items-center justify-center">
+            Loading shop...
+          </div>
+        }
+      >
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
 };
